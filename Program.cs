3bfbaf6bd6102ff09a7b1f62ddfdddc6b8b51939
Максим Ильин <mxmiljin@mail.ru>
2022-08-09 15:10:31 +0300

    task 6 completed

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число для проверки:");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+
+if (Number1 % 2 == 0)
+{
+    Console.WriteLine("Вы ввели четное число!");
+}
+else
+{
+    Console.WriteLine("Вы ввели нечетное число!");
+}
